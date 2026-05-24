89e79fe478ba386b3c2f7b4fb7e062231885dc27
PBL075 <pavelgorea75@gmail.com>
2026-05-24 17:50:30 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace try_catch
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Scrieti un numar care val cer");
+
+            try
+            {
+                int num = Convert.ToInt32(Console.ReadLine());
+            System.Console.WriteLine(num);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Scrieti ceea ce va cer");
+            }
+            finally
+            {
+                System.Console.WriteLine("");
+            }
+        }
+    }
+}
